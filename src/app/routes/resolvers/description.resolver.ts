4a0bae799971cfaaa876219831e2f82584e25318
Rob Dempsey <robd@nightlife.com.au>
2020-04-20 21:37:16 +1000

    Updates componentDescription

--- src/app/routes/resolvers/description.resolver.ts
+++ src/app/routes/resolvers/description.resolver.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
-import { Observable, BehaviorSubject, of } from 'rxjs';
+import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
+import { Observable, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class DescriptionResolver implements Resolve<string> {
     resolve(route: ActivatedRouteSnapshot): Observable<string> {
-        return of(`This data was fetched from a backend using http and configured using 
+        return of(`This data was fetched from a backend in DescriptionResolver using http and configured using 
         RouteDataInputs`)
     }
 }
