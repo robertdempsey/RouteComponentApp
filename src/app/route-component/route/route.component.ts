f0b5d09dd5428015b7596a327e6c1f26342d45d4
Rob Dempsey <robd@nightlife.com.au>
2020-05-04 20:21:41 +1000

    Removes unneccesary unsubscribe in route.component.ts

--- src/app/route-component/route/route.component.ts
+++ src/app/route-component/route/route.component.ts
@@ -1,8 +1,6 @@
-import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Route } from '@angular/router';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
-import { ComponentInjectorComponent, InjectionComponent } from '../../component-injector/component-injector.component';
+import { InjectionComponent } from '../../component-injector/component-injector.component';
 import { RouteService } from './route.service';
 
 export interface RouteData<RouteServiceType, ComponentType> extends Route {
@@ -16,10 +14,9 @@ export interface RouteData<RouteServiceType, ComponentType> extends Route {
   templateUrl: './route.component.html',
   styleUrls: ['./route.component.css']
 })
-export class RouteComponent<RouteServiceType, ComponentType> implements OnInit, OnDestroy {
+export class RouteComponent<RouteServiceType, ComponentType> implements OnInit {
 
   component: InjectionComponent<RouteServiceType, ComponentType>;
-  private destroyed$ = new Subject<void>();
 
   constructor(
     private route: ActivatedRoute,
@@ -27,9 +24,7 @@ export class RouteComponent<RouteServiceType, ComponentType> implements OnInit, 
   ) { }
 
   ngOnInit(): void {
-    this.route.data.pipe(
-      takeUntil(this.destroyed$)
-    ).subscribe((data: RouteData<RouteServiceType, ComponentType>['data']) => {
+    this.route.data.subscribe((data: RouteData<RouteServiceType, ComponentType>['data']) => {
       for (let output in data.component.outputs) {
         data.component.outputs[output] = data.component.outputs[output].bind(this)
       }
@@ -37,10 +32,5 @@ export class RouteComponent<RouteServiceType, ComponentType> implements OnInit, 
       this.component = data.component;
     })
 
-  }
-
-  ngOnDestroy(): void {
-    this.destroyed$.next();
-    this.destroyed$.complete();
   }
 }
